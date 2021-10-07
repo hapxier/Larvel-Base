1e16cd954535a3bd24fef365455d7e18f7d495e5
Luis  R. Salgado <luis_rafael_salgado@yahoo.es>
2021-10-06 23:46:24 -0600

    Update Handler.php

--- blog/app/Exceptions/Handler.php
+++ blog/app/Exceptions/Handler.php
@@ -50,6 +50,22 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if($exception instanceof ModelNotFoundException){
+            return response()->json(["res" => false, "error" => "Error de modelo"], 400);
+        }
+
+        if($exception instanceof QueryException){
+            return response()->json(["res" => false, "message" => "Error de consulta BDD " , $exception->getMessage()], 500);
+        }
+
+        if($exception instanceof HttpException){
+            return response()->json(["res" => false, "message" => "Error de ruta"], 404);
+        }
+
+        if($exception instanceof AuthenticationException){
+            return response()->json(["res" => false, "message" => "Error de autenticación"], 401);
+        }
+        
         return parent::render($request, $exception);
     }
 }
